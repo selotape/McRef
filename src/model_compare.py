4ef16f9dc7d7f999d7783365cc57f6ef5c1e1575
selotape <RonVisbord@Gmail.com>
2015-12-18 20:55:16 +0200

    renamed directories, added idea files and slightly updated algorithm to calculate final result

--- src/model_compare.py
+++ src/model_compare.py
@@ -1,17 +1,25 @@
-get_ipython().magic('matplotlib inline')
+
+# coding: utf-8
+
+# In[13]:
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-pd.set_option('display.mpl_style', 'default') 
+from numpy import exp
+from pandas.core.series import Series
+
+pd.set_option('display.mpl_style', 'default')
 pd.set_option('display.width', 5000) 
 pd.set_option('display.max_columns', 60)
 
 
+# In[17]:
 
-flat_coalescence_stats =     pd.read_csv        ('../logs/sample-data.flatStats.tsv',         sep='\t')
+flat_coalescence_stats =     pd.read_csv        ('C:\\Users\\ronvis\\Dropbox\\Thesis\\ModelCompare\\code\\ModelCompare\\sample_data\\sample-data.flatStats.tsv', sep='\t')
 
 
-
+# In[18]:
 
 flat_coalescence_stats = flat_coalescence_stats[['logPrior','coalStatFlat', 'numCoalFlat', 'root_theta', 'genealogyLogLikelihood' ]]
 flat_coalescence_stats.columns = ['logPrior','time_stats', 'num_coal', 'root_ϴ', 'P_Z_ϴM' ]
@@ -19,18 +27,15 @@ flat_coalescence_stats.columns = ['logPrior','time_stats', 'num_coal', 'root_ϴ'
 printFactor = 1000.0
 flat_coalescence_stats['root_ϴ'] = flat_coalescence_stats[['root_ϴ']].apply(lambda x:x/printFactor)
 
-flat_coalescence_stats[:5]
 
-
-
+# In[19]:
 
 def P_Z_ϴM0(theta, num_coal, time_stats):
     result = num_coal*np.log(2.0/theta) -(time_stats/theta)
     return result
-P_Z_ϴM0(0.00282063, 7000, 3.442481)
 
 
-
+# In[20]:
 
 flat_coalescence_stats['root_ϴ'] = flat_coalescence_stats[['root_ϴ']].apply(lambda x:x/1000.0)
 root_ϴ = flat_coalescence_stats['root_ϴ'] 
@@ -39,10 +44,28 @@ time_stats = flat_coalescence_stats['time_stats']
 
 flat_coalescence_stats['P_Z_ϴM0'] = P_Z_ϴM0(root_ϴ, num_coal, time_stats)
 
-flat_coalescence_stats.sort_values(by='P_Z_ϴM0', ascending=False)[:5]
+
+# In[21]:
+
+flat_coalescence_stats['P_Z_ϴM0'].plot()
 
 
-# In[ ]:
+# In[22]:
+
+flat_coalescence_stats['P_Z_ϴM'].plot()
 
 
+# In[23]:
 
+flat_coalescence_stats.describe()
+
+
+# In[24]:
+
+flat_coalescence_stats.head()
+
+E_P_Z_ϴM = flat_coalescence_stats['P_Z_ϴM'].mean()
+E_P_Z_ϴM0 = flat_coalescence_stats['P_Z_ϴM0'].mean()
+
+result = E_P_Z_ϴM - E_P_Z_ϴM0
+print(result)
