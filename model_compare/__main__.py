ee78834104520f197f989c710e0c3541a3aa973a
selotape <RonVisbord@Gmail.com>
2015-12-26 01:00:03 +0200

    reorganized and reexecuted experiment

--- model_compare/__main__.py
+++ model_compare/__main__.py
@@ -5,21 +5,19 @@ import configparser
 ### SETUP ###
 
 def main():
-    pd.set_option('display.mpl_style', 'default')
-    pd.set_option('display.width', 5000)
-    pd.set_option('display.max_columns', 60)
+    set_pandas()
 
     config = configparser.ConfigParser()
     config.read('config.ini')
-    simulation_name = config['Simulation']['name']
-    theta_column_name = config['Simulation']['theta_column']
-
-    flat_stats_path = '..\\simulations\\' + simulation_name + '\\gphocs\\data.flatStats.tsv'
-    trace_path = '..\\simulations\\' + simulation_name + '\\gphocs\\data.trace.tsv'
-
+    simulation_name = config.get('Simulation', 'name')
+    theta_column_name = config.get('Simulation','theta_column')
     print_factor = config.getint('Simulation','print_factor')
 
+
+    flat_stats_path = '..\\simulations\\' + simulation_name + '\\gphocs\\data.flatStats.tsv'
     flat_stats = pd.read_csv(flat_stats_path, sep='\t')
+
+    trace_path = '..\\simulations\\' + simulation_name + '\\gphocs\\data.trace.tsv'
     trace = pd.read_csv(trace_path, sep='\t')
 
     flat_stats[theta_column_name] = trace[theta_column_name]
@@ -29,10 +27,7 @@ def main():
     flat_stats.columns = ['logPrior', 'time_stats', 'num_coal', theta_column_name, 'P_Z_ϴM']
 
 
-
     flat_stats[theta_column_name] = flat_stats[[theta_column_name]].apply(lambda x: x / print_factor)
-
-
 
 
     thetas = flat_stats[theta_column_name]
@@ -41,8 +36,21 @@ def main():
 
     flat_stats['P_Z_ϴM0'] = P_Z_ϴM0(thetas, num_coal, time_stats)
 
+
+    # print to make sense
+    flat_stats['P_Z_ϴM0'] += + 194000
+    flat_stats[['P_Z_ϴM0', 'P_Z_ϴM']].plot()
     print(flat_stats.head())
 
+
+
+
+
+
+def set_pandas():
+    pd.set_option('display.mpl_style', 'default')
+    pd.set_option('display.width', 5000)
+    pd.set_option('display.max_columns', 60)
 
 def P_Z_ϴM0(theta, num_coal, time_stats):
     result = num_coal*np.log(2.0/theta) -(time_stats/theta)
