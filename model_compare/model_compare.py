741bb8dcf2451d2b7742fb73f0bb2df86418fc9d
selotape <RonVisbord@Gmail.com>
2016-01-09 19:10:13 +0200

    added cli input to model_compare rearranged dirstructure. renamed files.

--- model_compare/model_compare.py
+++ model_compare/model_compare.py
@@ -3,21 +3,21 @@ import sys
 import numpy as np
 import pandas as pd
 import configparser
+import os
 
 
 
-def main():
+def model_compare(simulation='sample'):
 
     set_pandas()
 
-    config_path = get_config_path()
-
     config = configparser.ConfigParser()
-    config.read(config_path)
+    config.read('config.ini')
 
-    simulation_name = config.get('Input', 'simulation_name')
     simulations_path = config.get('Input','simulations_path')
-    simulation_path = simulations_path + '\\' + simulation_name
+    simulation_path = simulations_path + '\\' + simulation
+
+    validate_simulation(simulation_path)
 
     flat_stats_name = config.get('Input','flat_stats_file_name')
     flat_stats_path = simulation_path + '\\' + flat_stats_name
@@ -31,17 +31,19 @@ def main():
     print_factor = config.getfloat('Data','print_factor')
     expectation_tail_length = config.getint('Data','expectation_tail_length')
 
-    results_name = config.get('Output','results_name')
-    results_path = simulation_path + '\\' + results_name
+    results_directory_path = simulation_path + '\\' + config.get('Output','results_directory')
+
+    trace_results_name = config.get('Output','trace_results_name')
+    trace_results_path = results_directory_path + '\\' + trace_results_name
 
     summary_name = config.get('Output','summary_name')
-    summary_path = simulation_path + '\\' + summary_name
+    summary_path = results_directory_path + '\\' + summary_name
 
     likelihoods_plot_name = config.get('Output','likelihoods_plot_name')
-    likelihoods_plot_path = simulation_path + '\\' + likelihoods_plot_name
+    likelihoods_plot_path = results_directory_path + '\\' + likelihoods_plot_name
 
-    expectance_plot_name = config.get('Output','expectance_plot_name')
-    expectance_plot_path = simulation_path + '\\' + expectance_plot_name
+    expectation_plot_name = config.get('Output','expectation_plot_name')
+    expectation_plot_path = results_directory_path + '\\' + expectation_plot_name
 
 
 
@@ -60,27 +62,19 @@ def main():
 
     flat_stats['E_ratio'] = flat_stats['P_Z_M'] - flat_stats['P_Z_M0'] # since likelihoods are stored in log-space, ratio is calculated using subtraction
 
-    save_results(expectation_tail_length, likelihoods_plot_path, expectance_plot_path, flat_stats, results_path, simulation_name, summary_path)
+    save_results(expectation_tail_length, likelihoods_plot_path, expectation_plot_path, flat_stats, trace_results_path, simulation, summary_path)
 
 
-def save_results(expectation_tail_length, likelihoods_plot_path, expectance_plot_path, flat_stats, results_path, simulation_name, summary_path):
+def save_results(expectation_tail_length, likelihoods_plot_path, expectation_plot_path, flat_stats, results_path, simulation_name, summary_path):
 
     flat_stats_tail = flat_stats[-expectation_tail_length:]
     save_plot(flat_stats_tail[['P_Z_M0', 'P_Z_M']], likelihoods_plot_path, simulation_name)
-    save_plot(flat_stats_tail[['E_ratio']], expectance_plot_path, simulation_name)
+    save_plot(flat_stats_tail[['E_ratio']], expectation_plot_path, simulation_name)
     flat_stats.to_csv(results_path)
     E = E_P_Z(flat_stats_tail['E_ratio'])
 
     f = open(summary_path, 'w')
     f.write("E={0}\n".format(E))
-
-
-def get_config_path():
-    if len(sys.argv) > 1:
-        config_path = sys.argv[1]
-    else:
-        config_path = 'config.ini'
-    return config_path
 
 
 def set_pandas():
@@ -102,11 +96,16 @@ def E_P_Z(likelihoods):
 
     return result
 
-def save_plot(data_frame, plot_save_path, plot_name=""):
+def save_plot(data_frame, plot_save_path, plot_name=''):
     plot = data_frame.plot(title=plot_name)
     figure = plot.get_figure()
     figure.savefig(plot_save_path)
 
 
+def validate_simulation(simulation_path):
+    pass
+
 if __name__ == "__main__":
-    main()
+    simulation_names = sys.argv[1:]
+    for simulation in simulation_names:
+        model_compare(simulation)
