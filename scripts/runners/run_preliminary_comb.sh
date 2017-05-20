afff6ff70e22ca934167a205299c499f9c6a491b
selotape <RonVisbord@Gmail.com>
2017-05-20 16:55:26 +0300

    added preliminary_comb experiment runner

---
+++ scripts/runners/run_preliminary_comb.sh
@@ -0,0 +1,9 @@
+model_compare="python /home/rvisbord/dev/modelcompare/run_model_compare.py"
+
+for exp in 'AB_C' 'BC_A' 'AC_B'; 
+do 
+    sim_dir="simulations/experiments/preliminary_comb/M3.15.migAC_0_0/${exp}"
+    time nohup $model_compare ${sim_dir} &
+    disown -a
+done
+
