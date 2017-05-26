3e6359763c1eafa55d381d11d7079234eb121525
selotape <RonVisbord@Gmail.com>
2017-05-26 12:58:11 +0300

    improved print script

--- scripts/printers/print_preliminary_comb_latest_results.sh
+++ scripts/printers/print_preliminary_comb_latest_results.sh
@@ -3,14 +3,16 @@
 
 function print_latest_results {
     dir=$1
-    last_experiment=(`ls -l "$dir/results" | awk '{print $9}' | sort -r | head -n 1`)
-    last_summary_path="$dir/results/$last_experiment/summary.txt"
-    cat "$last_summary_path"
+    if [ -d "${dir}" ]; then
+        last_experiment=(`ls -l "${dir}/results" | awk '{print $9}' | sort -r | head -n 1`)
+        last_summary_path="$dir/results/$last_experiment/summary.txt"
+        cat "$last_summary_path"
+    fi
 }
 
 
-for dir in `ls`; 
+for dir in `ls $1`; 
 do 
-    print_latest_results $dir;
+    print_latest_results "$1/$dir";
 done;
 
