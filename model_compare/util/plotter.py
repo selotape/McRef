db4cc3a2260689d6c8d12c663957b7c5f12c106f
selotape <RonVisbord@Gmail.com>
2017-06-10 15:36:01 +0300

    fix portability issue in matplotlib and improve image box

--- model_compare/util/plotter.py
+++ model_compare/util/plotter.py
@@ -1,12 +1,14 @@
+import matplotlib
+matplotlib.use('agg')
 import pandas as pd
 
 
 def save_plot(data_frame: pd.DataFrame, save_path: str, title: str):
     plot = data_frame.plot(title=title)
     plot_figure = plot.get_figure()
-    plot_figure.savefig(save_path + ".line.png")
+    plot_figure.savefig(save_path + ".line.png", bbox_inches='tight')
 
     # hist = data_frame.plot.hist(alpha=0.5, bins=160, normed=True,
     #                             title=plot_name + ' histogram')
     # hist_figure = hist.get_figure()
-    # hist_figure.savefig(plot_save_path + ".hist.png")
+    # hist_figure.savefig(plot_save_path + ".hist.png", bbox_inches='tight')
