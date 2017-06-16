880d0cc893a37919644a74f1e4b1ad9a7b7af675
selotape <RonVisbord@Gmail.com>
2017-06-17 00:33:21 +0300

    completed current setup script && git push

--- scripts/setup/setup_exp_dirs.sh
+++ scripts/setup/setup_exp_dirs.sh
@@ -1,26 +1,36 @@
-for hyp in AB_C BC_A AC_B ; do
+#!/usr/bin/env bash
+set -x
+set -o errexit
+set -o nounset
 
-  pushd /home/rvisbord/experiments/comb 
-  for sim in  M3.20.migAC_0_0 M3.25.migAC_0_0 M3.29.migAC_0_0 ; 
+pushd /home/rvisbord/experiments/comb 
+  
+for sim in  M3.20.migAC_0_0   M3.25.migAC_0_0   M3.29.migAC_0_0 ; do 
 
-  do 
+  rm -rf ${sim}
+  mkdir -p ${sim}
+  pushd ${sim}
 
-    mkdir -p ${sim}/${hyp};
+  cp /home/igronau/share/sims/simM3/${sim}/seqs.txt.gz ./
+  gunzip ./seqs.txt.gz
 
-    cp /home/igronau/share/sims/simM3/${sim}/seqs.txt.gz ./${sim}
-    pushd ./${sim}
-    gunzip seqs.txt.gz
+  for hyp in AB_C BC_A AC_B ; do 
+   
+    rm -rf ${hyp} 
+    mkdir -p ${hyp};
+    pushd ${hyp}
+
+    cp ../../M3.15.migAC_0_0/with_clade_and_mem_fix_without_assertions/${hyp}/${hyp}_control-file.ctl ./
+    sed -i -e "s/\/home\/igronau\/share\/sims\/simM3\/M3.15.migAC_0_0\/seqs.txt/ \/home\/rvisbord\/experiments\/comb\/${sim}\/seqs.txt /g" ./${hyp}_control-file.ctl;
+
+    echo `pwd`
+    nohup G-PhoCS ./${hyp}_control-file.ctl &
+    
     popd
 
-    cp M3.15.migAC_0_0/with_clade_and_mem_fix_without_assertions/${hyp}/${hyp}_control-file.ctl ${sim}/${hyp} 
-    sed -i -e "s/\/home\/igronau\/share\/sims\/simM3\/M3.15.migAC_0_0\/seqs.txt/ \/home\/rvisbord\/experiments\/comb\/${sim}\/seqs.txt /g" ${sim}/${hyp}/${hyp}_control-file.ctl;
-
-    pushd ./${sim}/${hyp}
-    G-PhoCS ./${hyp}_control-file.ctl &
-    popd
   done;
 
+  popd
+
 done
-
-
 popd
