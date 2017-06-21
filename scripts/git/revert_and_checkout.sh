ee4d1e5acdc77568789160dc5f51abc8c3f21d46
selotape <RonVisbord@Gmail.com>
2017-06-21 23:31:51 +0300

    reordered setup files in preperation for comb mig sanity tests

--- scripts/git/revert_and_checkout.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-git clean -f && git clean -fd && git checkout . && git pull
---
+++ scripts/setup_runs/setup_gphocs_migbands_sanity.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -x
+set -o errexit
+set -o nounset
+
+root=/home/rvisbord/experiments/comb_with_migs
+mkdir -p ${root}
+pushd ${root}
+  
+for sim in M3.00.migAC_0_0  M3.05.migAC_0_1  M3.05.migAC_0_2  M3.05.migAC_0_4  M3.10.migAC_0_1  M3.10.migAC_0_2  M3.10.migAC_0_4  M3.15.migAC_0_1  M3.15.migAC_0_2  M3.15.migAC_0_4 ; do
+
+  rm -rf ${sim}
+  mkdir -p ${sim}
+  pushd ${sim}
+
+  for hyp in AB_C BC_A AC_B ; do 
+
+    mig_hyp="${hyp}__A-\>C"
+    rm -rf ${mig_hyp}
+    mkdir -p ${mig_hyp};
+    pushd ${mig_hyp}
+
+    # copy a ready-made control file and edit it
+    old_control_file=/home/rvisbord/experiments/comb/M3.15.migAC_0_4/${mig_hyp}/control-file.ctl
+    control_file=${sim}__${mig_hyp}_control-file.ctl
+    cp ${old_control_file} ./${control_file}
+    sed -i -e "s/M3.15.migAC_0_4/${sim}/g" ${control_file}
+
+    # run gphocs
+    echo "currently in:`pwd`"
+    echo "nohup G-PhoCS ${current_control_file} &"
+    
+    popd
+
+
+  done;
+
+  popd
+
+done
+popd
