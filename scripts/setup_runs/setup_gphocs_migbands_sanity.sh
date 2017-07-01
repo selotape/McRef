76333d9c55170e9249604a8f2905340f3edae48b
selotape <RonVisbord@Gmail.com>
2017-07-01 10:47:09 +0300

    finished migband setup script

--- scripts/setup_runs/setup_gphocs_migbands_sanity.sh
+++ scripts/setup_runs/setup_gphocs_migbands_sanity.sh
@@ -15,20 +15,36 @@ for sim in  M3.05.migAC_0_1  M3.05.migAC_0_2  M3.05.migAC_0_4  M3.10.migAC_0_1  
   for hyp in AB_C BC_A AC_B ; do 
 
     mig_hyp="${hyp}__C->A"
-    rm -rf ${mig_hyp}
-    mkdir -p ${mig_hyp};
-    pushd ${mig_hyp}
+#    rm -rf ${mig_hyp}
+#    mkdir -p ${mig_hyp};
+#    pushd ${mig_hyp}
 
     # copy a ready-made control file and edit it
-    old_control_file=/home/rvisbord/experiments/comb/M3.15.migAC_0_4/${mig_hyp}/control-file.ctl
-    control_file=${sim}__${mig_hyp}_control-file.ctl
+#    old_control_file=/home/rvisbord/experiments/comb/M3.15.migAC_0_4/${mig_hyp}/control-file.ctl
+#    control_file=${sim}__${mig_hyp}_control-file.ctl
+#    cp ${old_control_file} ./${control_file}
+#    sed -i -e "s/M3.15.migAC_0_4/${sim}/g" ${control_file}
+
+    # run gphocs
+#    echo "nohup G-PhoCS ${control_file} &"
+#          nohup G-PhoCS ${control_file} &
+    
+#    popd
+
+
+    mkdir -p ${hyp}
+    pushd ${hyp}
+
+    old_control_file=/home/rvisbord/experiments/comb/M3.15.migAC_0_0/with_clade_and_mem_fix_without_assertions/${hyp}/${hyp}_control-file.ctl
+    control_file=${sim}__${hyp}_control-file.ctl
     cp ${old_control_file} ./${control_file}
     sed -i -e "s/M3.15.migAC_0_4/${sim}/g" ${control_file}
 
-    # run gphocs
+    pwd
     echo "nohup G-PhoCS ${control_file} &"
           nohup G-PhoCS ${control_file} &
-    
+
+
     popd
 
 
