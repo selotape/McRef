8d15815d5f0207446f04d0108971953fa9319ffb
selotape <RonVisbord@Gmail.com>
2017-08-26 19:44:51 +0300

    implement argparse

--- run_model_compare.py
+++ run_model_compare.py
@@ -1,39 +1,19 @@
+import argparse
 import os
-import sys
 
 from model_compare import compare_models
 from model_compare.util.general_purpose import timed
 
 
 @timed
-def main(args):
-    simulation, is_clade = _parse_args(args)
-    compare_models(simulation, is_clade)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--clade", help="enable clade reference model. ", action="store_true")
+    parser.add_argument("simulations", nargs='+', help="space-delimited list of directories containing model_compare experiments")
+    args = parser.parse_args()
 
-
-def _parse_args(args):
-    is_clade = False
-    simulation = None
-    if len(args) == 0:
-        print_usage_and_exit()
-    else:
-        simulation = args[0]
-    if len(args) == 2:
-        if args[1] != '--clade':
-            print_usage_and_exit(err_msg="'%s' is not a valid argument" % args[1])
-        else:
-            is_clade = True
-
-    if not is_valid_simulation(simulation):
-        print_usage_and_exit(err_msg="'%s' is not a valid simulation dir" % simulation)
-
-    return simulation, is_clade
-
-
-def print_usage_and_exit(err_msg=''):
-    if err_msg: print(err_msg, file=sys.stderr)
-    print(r"Usage: >>> python ./run_model_compare.py ./path/to/config_dir [--clade]")
-    exit()
+    for simulation in args.simulations:
+        compare_models(simulation, args.clade)
 
 
 def is_valid_simulation(sim):
@@ -41,4 +21,4 @@ def is_valid_simulation(sim):
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
