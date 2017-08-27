bab11712b0757b9962993528c94e81b7c1d65293
selotape <RonVisbord@Gmail.com>
2017-08-27 10:40:39 +0300

    implement log rotation and tqdm progress bar

--- model_compare/util/log.py
+++ model_compare/util/log.py
@@ -1,7 +1,8 @@
 import logging
 import sys
 from functools import wraps
-from logging import FileHandler, StreamHandler
+from logging import StreamHandler
+from logging.handlers import RotatingFileHandler
 
 DEFAULT_FORMAT = "[%(asctime)s][pid:%(process)d][%(levelname)s][%(name)s] %(message)s"
 
@@ -14,7 +15,7 @@ def configure_logging(log_level, log_file, fmt=DEFAULT_FORMAT):
     logger = logging.getLogger()  # root
     logger.setLevel(log_level)
     formatter = logging.Formatter(fmt=fmt)
-    handler = FileHandler(log_file) if log_file else StreamHandler(sys.stdout)
+    handler = RotatingFileHandler(log_file, maxBytes=1024 * 1024, backupCount=2) if log_file else StreamHandler(sys.stdout)
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
@@ -25,7 +26,7 @@ def with_entry_log(l):
         def logged(*args, **kwargs):
             l.info("\n===== Starting! =====")
             result = f(*args, **kwargs)
-            l.info("===== Done! =====\n")
+            l.info("\n===== Done! =====")
             return result
 
         return logged
--- run_model_compare.py
+++ run_model_compare.py
@@ -2,6 +2,8 @@ import argparse
 from concurrent.futures import ProcessPoolExecutor
 from functools import partial
 
+from tqdm import tqdm
+
 from model_compare import compare_models, Result
 from model_compare.util.general_purpose import timed
 from model_compare.util.log import module_logger, tee_log
@@ -15,25 +17,27 @@ def main():
 
     configured_compare_models = partial(compare_models, is_clade=args.clade)
 
-    with ProcessPoolExecutor() as executor:
+    with ProcessPoolExecutor(max_workers=args.max_workers) as executor:
         results = executor.map(configured_compare_models, args.simulations)
-        _print_results(results)
+        _print_results(results, args.simulations)
 
 
 def _parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("--clade", help="enable clade reference model", action="store_true")
+    parser.add_argument("--max-workers", help="maximum number of processes to spawn. Default is #available-cpu", type=int, default=None)
     parser.add_argument("simulations", nargs='+', help="space-delimited list of directories containing model_compare experiments")
     return parser.parse_args()
 
 
-def _print_results(results):
-    header = ','.join(Result._fields)
+def _print_results(results, simulations):
+    simulation_results = [ret for ret in tqdm(results, total=len(simulations))]
 
+    header = ','.join(Result._fields)
     print("Summary:")
     print(header)
-    for result in results:
-        tee_log(_log.info, ','.join(str(f) for f in result))
+    for sim_res in simulation_results:
+        tee_log(_log.info, ','.join(str(f) for f in sim_res))
 
 
 if __name__ == "__main__":
