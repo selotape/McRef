ac04899897bf9cce8e9334d8f21320e166fb6c43
selotape <RonVisbord@Gmail.com>
2017-12-23 11:26:01 +0200

    finished rearranging

--- model_compare/tests/test_tau_bounds.py
+++ model_compare/tests/test_tau_bounds.py
@@ -2,7 +2,7 @@ from math import isclose
 
 import pytest
 
-from thesis_pseudo_code.tau_bounds.algorithm import ancestors, lca, find_tau_bounds, Population, Event, descendants
+from thesis_pseudo_code.tau_bounds import ancestors, lca, find_tau_bounds, Population, Event, descendants
 
 
 def test_ancestors():
