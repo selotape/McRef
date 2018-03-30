fea4ed776e07e8c6d56a0ee06309a4e1dbd18567
selotape <RonVisbord@Gmail.com>
2018-03-30 19:19:21 +0300

    improve results printing

--- mcref/__main__.py
+++ mcref/__main__.py
@@ -3,7 +3,9 @@ import os
 import sys
 from concurrent.futures import ProcessPoolExecutor
 from multiprocessing import cpu_count
+from typing import List
 
+from tabulate import tabulate
 from tqdm import tqdm
 
 sys.path.append(os.getcwd())
@@ -12,7 +14,9 @@ from mcref.model_compare import run_simulation, Result
 from mcref.util.general_purpose import timed
 from mcref.util.log import module_logger, tee_log
 
+
 _log = module_logger(__name__)
+
 
 
 @timed
@@ -20,23 +24,21 @@ def main():
     args = _parse_arguments()
 
     with ProcessPoolExecutor(max_workers=cpu_count()) as executor:
-        results = executor.map(run_simulation, args.simulations)
-        _print_results(results, args.simulations)
+        futures = executor.map(run_simulation, args.simulations)
+        results = list(tqdm(futures, total=len(args.simulations)))
+        _print_results(results)
 
 
 def _parse_arguments():
     parser = argparse.ArgumentParser()
+    # parser.add_argument("--purge-results", help="Delete all previous simulation results", action="store_true") # TODO
     parser.add_argument("simulations", nargs='+', help="space-delimited list of directories containing model_compare experiments")
     return parser.parse_args()
 
 
-def _print_results(results, simulations):
-    simulation_results = list(tqdm(results, total=len(simulations)))
-    header = ','.join(Result._fields)
-    print("Summary:")
-    print(header)
-    for sim_res in simulation_results:
-        tee_log(_log.info, ','.join(str(f) for f in sim_res))
+def _print_results(results: List[Result]):
+    results_table = tabulate(results, headers=Result._fields)
+    tee_log(_log.info, '\n' + results_table)
 
 
 if __name__ == "__main__":
