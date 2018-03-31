7886428f1bbb386363649afbda56feea0287bd6b
selotape <RonVisbord@Gmail.com>
2018-03-31 12:32:02 +0300

    support old-results-purging

--- mcref/__main__.py
+++ mcref/__main__.py
@@ -13,15 +13,18 @@ sys.path.append(os.getcwd())
 from mcref.model_compare import run_simulation, Result
 from mcref.util.general_purpose import timed
 from mcref.util.log import module_logger, tee_log
-
+import shutil
 
 _log = module_logger(__name__)
-
 
 
 @timed
 def main():
     args = _parse_arguments()
+
+    if args.purge_results:
+        for sim in args.simulations:
+            _purge_results(sim)
 
     with ProcessPoolExecutor(max_workers=cpu_count()) as executor:
         futures = executor.map(run_simulation, args.simulations)
@@ -31,9 +34,15 @@ def main():
 
 def _parse_arguments():
     parser = argparse.ArgumentParser()
-    # parser.add_argument("--purge-results", help="Delete all previous simulation results", action="store_true") # TODO
+    parser.add_argument("--purge-results", help="Delete all previous simulation results", action="store_true")
     parser.add_argument("simulations", nargs='+', help="space-delimited list of directories containing model_compare experiments")
     return parser.parse_args()
+
+
+def _purge_results(sim):
+    results_dir = os.path.join(sim, 'results')
+    tee_log(_log.info, 'Deleting directory %s' % results_dir)
+    shutil.rmtree(results_dir, ignore_errors=True)
 
 
 def _print_results(results: List[Result]):
